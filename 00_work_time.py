fefb6d1443b31d5a17550379fc3cc5af3e42a6b7
Perchik <ilya1880@mail.ru>
2024-10-01 10:04:38 +0300

    Доб. карусель. Всё работает.

--- 00_work_time.py
+++ 00_work_time.py
@@ -1,9 +1,10 @@
 import time
-from logging import currentframe
+
 
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.carousel import Carousel
 from kivy.lang.builder import Builder
 from kivy.properties import ListProperty,StringProperty
 
@@ -15,24 +16,22 @@ month_lst = ['Январь', 'Февраль', 'Март', 'Апрель', 'Ма
 time_now = time.time() # Секунды с начала эпохи
 time_day = time.localtime(time_now) # Текущее число
 CURRENT_DAY = time.strftime("%d", time_day)
-number_month = int(time.strftime("%m", time_day)[1])
+number_month = int(time.strftime("%m", time_day))
 CURRENT_MONTH = month_lst[number_month - 1]
 CURRENT_HOURS = time.strftime("%H", time_day)
 CURRENT_MINUTES = time.strftime("%M", time_day)
 
-class Container(Widget):
+class Pages(Carousel):
     hours_spinner_str = StringProperty(CURRENT_HOURS)
     minutes_spinner_str = StringProperty(CURRENT_MINUTES)
     day_spinner_str = StringProperty(CURRENT_DAY)
     month_spinner_str = StringProperty(CURRENT_MONTH)
-    month_lst_property = ListProperty(month_lst) # Устан.всех месяцев в Spinner
-    label_month_lst_property = ListProperty([CURRENT_DAY, CURRENT_MONTH]) # Устан.даты в Label
+    month_lst_property = ListProperty(month_lst)  # Устан.всех месяцев в Spinner
+    label_month_lst_property = ListProperty([CURRENT_DAY, CURRENT_MONTH])  # Устан.даты в Label
 
     def __init__(self, **kwargs):
-        super(Container, self).__init__(**kwargs)
-        self._day = ''
-        self._month = ''
-        print(self.label_month_lst_property)
+        super(Pages, self).__init__(**kwargs)
+        print(self.ids.keys())
 
     def create_date(self):
         self.label_month_lst_property.clear()
@@ -43,17 +42,9 @@ class Container(Widget):
 
 
 
-
-    def func_slider(self,value):
-        self.ids.lab.text=str(value[1])
-        self.ids.lab.font_size =value[1]
-    def func_file_chooser(self,path_file):
-        self.ids.imagefile.source = path_file[0]
-        print(path_file)
-
 class MyApp(App):
     def build(self):
-        return Container()
+        return Pages()
 
 
 if __name__ == '__main__':
