aebe358154a55f01937002da846a3cb29339b797
Perchik <ilya1880@mail.ru>
2024-10-01 18:00:15 +0300

    Обозначил Spinner переменные класса

--- 00_work_time.py
+++ 00_work_time.py
@@ -26,6 +26,7 @@ CURRENT_HOURS = time.strftime("%H", time_day)
 CURRENT_MINUTES = time.strftime("%M", time_day)
 
 class Pages(Carousel):
+    """Читай переменные. Их имена обо всём говорят."""
     hours_start_work = StringProperty("HStW")
     minutes_start_work_minutes = StringProperty("MinStW")
 
@@ -39,20 +40,28 @@ class Pages(Carousel):
     hours_end_lanch = StringProperty("HEndLanch")
     minutes_end_lunch = StringProperty("MinStlanch")
 
-    minutes_spinner_str = StringProperty("00")
     day_spinner_str = StringProperty(CURRENT_DAY)
     month_spinner_str = StringProperty(CURRENT_MONTH)
+
     month_lst_property = ListProperty(month_lst)  # Устан.всех месяцев в Spinner
     label_month_lst_property = ListProperty([CURRENT_DAY, CURRENT_MONTH])  # Устан.даты в Label
 
     def __init__(self, **kwargs):
         super(Pages, self).__init__(**kwargs)
-        # self._day = self.day_spinner_str
-        # self._month = self.month_spinner_str
-        self._hours_start_work = self.hours_start_work
-        self._minutes = self.minutes_start_work_minutes
-        """"""
-        self._hours_end_work = self.hours_end_work
+        _hours_start_work = self.hours_start_work
+        _minutes_start_work_minutes = self.minutes_start_work_minutes
+
+        _hours_end_work = self.hours_end_work
+        _minutes_end_work = self.minutes_end_work
+
+        _hours_start_lunch = self.hours_start_lunch
+        _minutes_start_lunch = self.minutes_start_lunch
+
+        _hours_end_lanch = self.hours_end_lanch
+        _minutes_end_lunch = self.minutes_end_lunch
+
+
+
 
     def create_start_work_time(self,spinner):
         if spinner.uid == 118:
